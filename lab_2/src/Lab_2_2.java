5b07d02475db6718c80cd45aeeec5d8bd8d20fd4
An1mE111n1k <Maratdzugaev095@gmail.com>
2025-04-23 13:21:26 +0300

    log2

--- lab_2/src/Lab_2_2.java
+++ lab_2/src/Lab_2_2.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 
 public class Lab_2_2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите значение x: ");
+        int x = scanner.nextInt();
+        boolean isSpecial = (x >= 10 && x < 30) || (x > 40 && x < 70);
+        System.out.println("isSpecial = " + isSpecial);
+        scanner.close();
     }
 }
